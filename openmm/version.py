014806a28c8fa5c7f0ef1bab38ae3f805316072c
ShaheerSyed <93398374+ShaheerSyed@users.noreply.github.com>
2023-12-03 16:29:41 -0500

    Add files via upload

---
+++ openmm/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM OPENMM SETUP.PY
+short_version = '8.1.0'
+version = '8.1.0'
+full_version = '8.1.0.dev-43f571d'
+git_revision = '43f571d90fc1c882848cbd9b55e097a1775a9075'
+release = False
+openmm_library_path = r'C:/Users/shahe/anaconda3/envs/pchem/Library/lib'
+
+if not release:
+    version = full_version
